6519dac10a7b05218b1ed16591522be54f959b36
Iwan Aucamp <aucampia@gmail.com>
2022-03-18 22:35:47 +0100

    Some improvements to devtools-golang-v1beta1
    - Setup golangci config so it can be overwritten by users.
    - Add build target which just compiles things, and make this part of the
      default targets.
    - Generate `coverage.out` on test
    - Add a `README.md` to  `images/devtools-golang-v1beta1/tests/prototype/`
      so it makes for a better example.

--- images/devtools-golang-v1beta1/tests/test_image.py
+++ images/devtools-golang-v1beta1/tests/test_image.py
@@ -8,14 +8,14 @@ import sys
 from contextlib import contextmanager
 from pathlib import Path, PurePath
 from pytest import CaptureFixture
-from textwrap import dedent
-from typing import Iterator, TypeVar, Union
+from typing import Dict, Iterator, List, Optional, TypeVar, Union
 
 TEST_DIR = Path(__file__).parent.absolute()
 PROTOTYPE_DIR = TEST_DIR / "prototype"
 
 PathLike = Union[PurePath, str]
 PathLikeT = TypeVar("PathLikeT", bound=PathLike)
+
 
 @contextmanager
 def ctx_prototype(tmp_path: Path) -> Iterator[Path]:
@@ -26,6 +26,7 @@ def ctx_prototype(tmp_path: Path) -> Iterator[Path]:
     with ctx_chdir(workdir):
         yield workdir
 
+
 @contextmanager
 def ctx_chdir(newdir: PathLikeT) -> Iterator[PathLikeT]:
     cwd = os.getcwd()
@@ -35,12 +36,21 @@ def ctx_chdir(newdir: PathLikeT) -> Iterator[PathLikeT]:
     finally:
         os.chdir(cwd)
 
+
 def test_prototype_ok() -> None:
     with ctx_chdir(PROTOTYPE_DIR):
         subprocess.run("docker-compose run --rm devtools".split(" "), check=True)
+        assert (PROTOTYPE_DIR / "coverage.out").exists()
 
-@pytest.mark.parametrize("code,error_msg", [
-    ("""\
+
+@pytest.mark.parametrize(
+    ["command", "success", "files", "expected_outputs"],
+    [
+        (
+            None,
+            False,
+            {
+                "bad_file.go": """\
 package main
 
 func NoDocs() int {
@@ -50,10 +60,15 @@ func NoDocs() int {
 func init() {
 	print(NoDocs())
 }
-""",
-    "exported function NoDocs should have comment or be unexported",
-    ),
-    ("""\
+"""
+            },
+            ["exported function NoDocs should have comment or be unexported"],
+        ),
+        (
+            None,
+            False,
+            {
+                "bad_file.go": """\
 package main
 
 import "fmt"
@@ -68,28 +83,57 @@ func init() {
 	print(x)
 }
 """,
-    "Error return value is not checked",
-    ),
-    ("""\
+            },
+            ["Error return value is not checked"],
+        ),
+        (
+            None,
+            False,
+            {
+                "bad_file.go": """\
 package main
 
 func UnusedFunc() int {
 	return 3
 }
-""",
-    "`UnusedFunc` is unused",
-    ),
-])
-def test_prototype_linter_errors(
+"""
+            },
+            ["`UnusedFunc` is unused"],
+        ),
+        (
+            None,
+            True,
+            {
+                ".golangci.yml": """\
+linters:
+  disable-all: true
+  enable: [ misspell ]
+"""
+            },
+            [""],
+        ),
+    ],
+)
+def test_alterations(
     tmp_path: Path,
     capfd: CaptureFixture[str],
-    code,
-    error_msg,
+    command: Optional[List[str]],
+    success: bool,
+    files: Optional[Dict[str, str]],
+    expected_outputs: List[str],
 ) -> None:
+    if command is None:
+        command = "docker-compose run --rm devtools".split(" ")
+    if files is None:
+        files = {}
     with ctx_prototype(tmp_path) as workdir:
-        (workdir / "bad_file.go").write_text(code)
-        with pytest.raises(subprocess.CalledProcessError):
-            subprocess.run("docker-compose run --rm devtools".split(" "), check=True)
+        for file_name, file_content in files.items():
+            (workdir / file_name).write_text(file_content)
+        if success:
+            subprocess.run(command, check=True)
+        else:
+            with pytest.raises(subprocess.CalledProcessError):
+                subprocess.run(command, check=True)
         captured = capfd.readouterr()
         with capfd.disabled():
             sys.stdout.write("captured.out:\n")
@@ -97,11 +141,14 @@ def test_prototype_linter_errors(
             sys.stderr.write("captured.err:\n")
             sys.stderr.write(captured.err)
 
-        assert error_msg in captured.out
+        for expected_output in expected_outputs:
+            assert expected_output in (captured.out + captured.err)
+
 
 def test_prototype_failing_tests(tmp_path: Path, capfd: CaptureFixture[str]) -> None:
     with ctx_prototype(tmp_path) as workdir:
-        (workdir / "sum/failing_test.go").write_text("""\
+        (workdir / "sum/failing_test.go").write_text(
+            """\
 package sum_test
 
 import "testing"
@@ -122,9 +169,11 @@ func TestThatFails(t *testing.T) {
 
         assert "FAIL: TestThatFails" in captured.out
 
+
 def test_prototype_validate_fix(tmp_path: Path) -> None:
     with ctx_prototype(tmp_path) as workdir:
-        (workdir / "bad_file.go").write_text("""\
+        (workdir / "bad_file.go").write_text(
+            """\
 package main
 
 import "fmt"
@@ -144,7 +193,9 @@ func init() {
 }
 """
         )
-        subprocess.run("docker-compose run --rm devtools validate-fix".split(" "), check=True)
+        subprocess.run(
+            "docker-compose run --rm devtools validate-fix".split(" "), check=True
+        )
 
         formatted_file = """\
 package main
