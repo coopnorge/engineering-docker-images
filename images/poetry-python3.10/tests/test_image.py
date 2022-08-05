c05ee79ff9da69092cded236845055a6a293e41e
Nikita Barskov <nikita.barskov@coop.no>
2022-08-05 09:20:20 +0200

    Use `startsWith` for `poetry-python3.*` images.

--- images/poetry-python3.10/tests/test_image.py
+++ images/poetry-python3.10/tests/test_image.py
@@ -21,12 +21,12 @@ def build_image(
 
 @pytest.fixture(scope="session")
 def expected_poetry_version() -> bytes:
-    return b"Poetry version 1.1.14\n"
+    return b"Poetry version 1.1"
 
 
 @pytest.fixture(scope="session")
 def expected_python_version() -> bytes:
-    return b"Python 3.10.5\n"
+    return b"Python 3.10"
 
 
 def test_image_should_return_a_correct_python_version(
@@ -40,10 +40,10 @@ def test_image_should_return_a_correct_python_version(
         command="poetry --version",
         remove=True,
     )
-    assert actual_poetry_version == expected_poetry_version
+    assert actual_poetry_version.startswith(expected_poetry_version)
     actual_python_version = docker_client.containers.run(
         build_image.id,
         command="python --version",
         remove=True,
     )
-    assert actual_python_version == expected_python_version
+    assert actual_python_version.startswith(expected_python_version)
