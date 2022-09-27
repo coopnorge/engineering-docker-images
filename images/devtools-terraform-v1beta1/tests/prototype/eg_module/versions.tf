9b1f516b1bf445cd713da6b72c6899c70297517f
Atze de Vries <atze.wiebe.de.vries@coop.no>
2022-09-27 13:28:07 +0200

    add versions to eg module

---
+++ images/devtools-terraform-v1beta1/tests/prototype/eg_module/versions.tf
@@ -0,0 +1,5 @@
+# https://www.terraform.io/docs/language/settings/index.html
+# https://www.terraform.io/docs/language/expressions/version-constraints.html
+terraform {
+  required_version = "~> 1.0"
+}
