a11ffc48a62a212a4f279de30f917cf27658e434
Niels Henrik Hagen <nhhagen@gmail.com>
2022-10-14 15:51:36 +0200

    Use make as a task runner (#402)
    Closes: coopnorge/github-workflow-techdocs#31

--- images/techdocs/tests/test_image.py
+++ images/techdocs/tests/test_image.py
@@ -1,3 +1,6 @@
+import os
+import pathlib
+
 import docker
 import pytest
 from docker.models.images import Image
@@ -19,6 +22,13 @@ def build_image(
     return image
 
 
+@pytest.fixture(scope="session")
+def volumes() -> dict[str, dict[str, str]]:
+    current_directory = pathlib.Path(__file__).parent.resolve()
+    prototype = os.path.abspath(f"{current_directory}/prototype/")
+    return {prototype: {"bind": "/content", "mode": "rw"}}
+
+
 @pytest.mark.parametrize(
     "cli_command,expected_ouput",
     [
@@ -28,7 +38,7 @@ def build_image(
         ("yq --version", b"yq (https://github.com/mikefarah/yq/) version 4."),
     ],
 )
-def test_image_should_return_a_correct_techdocs_cli_version(
+def test_image_should_return_a_correct_cli_tool_version(
     docker_client: docker.DockerClient,
     build_image: Image,
     cli_command: str,
@@ -40,3 +50,51 @@ def test_image_should_return_a_correct_techdocs_cli_version(
         remove=True,
     )
     assert actual_output.startswith(expected_ouput)
+
+
+def test_lint(
+    docker_client: docker.DockerClient,
+    build_image: Image,
+    volumes: dict[str, dict[str, str]],
+) -> None:
+    actual_output = docker_client.containers.run(
+        build_image.id,
+        command="lint DOCS_DIR=docs/",
+        volumes=volumes,
+        remove=True,
+    )
+    assert actual_output == b"markdownlint docs/\n"
+
+
+def test_linguistics_check(
+    docker_client: docker.DockerClient,
+    build_image: Image,
+    volumes: dict[str, dict[str, str]],
+) -> None:
+    actual_output = docker_client.containers.run(
+        build_image.id,
+        command='linguistics-check MARKDOWN_FILES="README.md docs/index.md"',
+        volumes=volumes,
+        remove=True,
+    )
+    assert (
+        actual_output
+        == b"vale README.md docs/index.md\n\xe2\x9c\x94 \x1b[31m0 errors\x1b[0m, \x1b[33m0 warnings\x1b[0m and \x1b[34m0 suggestions\x1b[0m in 2 files.\n"
+    )
+
+
+def test_build(
+    docker_client: docker.DockerClient,
+    build_image: Image,
+    volumes: dict[str, dict[str, str]],
+) -> None:
+    actual_output = docker_client.containers.run(
+        build_image.id,
+        command="build",
+        volumes=volumes,
+        remove=True,
+    )
+    assert (
+        b"info: Successfully generated docs from /content into /content/site using local mkdocs"
+        in actual_output
+    )
