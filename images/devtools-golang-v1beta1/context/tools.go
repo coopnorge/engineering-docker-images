2122d597f521e9897ac8335355ca23d46947bee5
Niels Henrik Hagen <nhhagen@gmail.com>
2023-10-16 21:49:05 +0200

    Replace github.com/golang/mock with go.uber.org/mock
    github.com/golang/mock is deprecated and suggests consumers to use
    go.uber.org/mock as a drop in replacement.

--- images/devtools-golang-v1beta1/context/tools.go
+++ images/devtools-golang-v1beta1/context/tools.go
@@ -8,8 +8,8 @@ import (
 	_ "github.com/abice/go-enum"                                // toolchain
 	_ "github.com/cortesi/modd/cmd/modd"                        // toolchain
 	_ "github.com/envoyproxy/protoc-gen-validate"               // toolchain
-	_ "github.com/golang/mock/mockgen"                          // toolchain
 	_ "github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc" // toolchain
+	_ "go.uber.org/mock/mockgen"                                // toolchain
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"           // toolchain
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"            // toolchain
 )
